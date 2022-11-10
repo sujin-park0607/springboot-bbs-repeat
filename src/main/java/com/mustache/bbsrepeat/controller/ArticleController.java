3ae597edf6fc3a59321c22384e0799c7d7231daf
sujin0607 <goweg0607@gmail.com>
2022-11-10 11:48:04 +0900

    feat: findById 구현

--- src/main/java/com/mustache/bbsrepeat/controller/ArticleController.java
+++ src/main/java/com/mustache/bbsrepeat/controller/ArticleController.java
@@ -5,9 +5,14 @@ import com.mustache.bbsrepeat.domain.dto.ArticleDto;
 import com.mustache.bbsrepeat.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -22,14 +27,44 @@ public class ArticleController {
 
     @GetMapping(value="/new")
     public String newArticleForm(){
-        return "article/new";
+        return "articles/new";
     }
+
+    @GetMapping("/list")
+    public String list(Model model){
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles",articles);
+        return "articles/list";
+    }
+
+    @GetMapping("")
+    public String index(){
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model){
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if(!optArticle.isEmpty()){
+            model.addAttribute("article",optArticle.get());
+            return "articles/show";
+        }else{
+            return "articles/error";
+        }
+    }
+
+
 
     @PostMapping(value="/posts")
     public String createArticle(ArticleDto form){
         log.info(form.toString());
-        Article article = form.toEntity();
-        articleRepository.save(article);
-        return "";
+
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        Article saved = articleRepository.save(articleEntity);
+        log.info(saved.toString());
+        return "redirect:/articles/"+saved.getId();
     }
 }
--- src/main/java/com/mustache/bbsrepeat/domain/entity/Article.java
+++ src/main/java/com/mustache/bbsrepeat/domain/entity/Article.java
@@ -3,6 +3,7 @@ package com.mustache.bbsrepeat.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -14,11 +15,21 @@ public class Article {
     @Id
     @GeneratedValue
     private Long id;
+
+
+    @Column
     private String title;
+
+    @Column
     private String contents;
 
     public Article(String title, String contents) {
         this.title = title;
         this.contents = contents;
+    }
+
+    @Override
+    public String toString(){
+        return "Article{" + "id=" + id + ", title='" + title + '\'' + ", contents='" + contents + '\'' + '}';
     }
 }
