07df0f5816c1d5b7f26adc212aeec0f560c78396
sujin0607 <goweg0607@gmail.com>
2022-11-10 11:04:17 +0900

    feat: setup

---
+++ src/main/java/com/mustache/bbsrepeat/BbsRepeatApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbsrepeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsRepeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustache/bbsrepeat/BbsRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbsrepeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
