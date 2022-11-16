f983a9d84f086bf4c8c40a24e35e8aef13ca9274
sujin0607 <goweg0607@gmail.com>
2022-11-16 11:36:25 +0900

    test: ArticleRestControllerTest 추가

---
+++ src/test/java/com/mustache/bbsrepeat/controller/ArticleRestControllerTest.java
@@ -0,0 +1,45 @@
+package com.mustache.bbsrepeat.controller;
+
+
+import com.mustache.bbsrepeat.domain.dto.ArticleDto;
+import com.mustache.bbsrepeat.service.ArticleService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ArticleRestController.class)
+class ArticleRestControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ArticleService articleService;
+
+    @Test
+    @DisplayName("Json형태로 Response가 잘 오는지")
+    void jsonResponse() throws Exception{
+        Long id = 1l;
+
+        given(articleService.getArticleById(id))
+                .willReturn(new ArticleDto(1l,"첫번째 글","글 내용입니다."));
+
+        mockMvc.perform(get("/api/v1/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.contents").exists())
+                .andDo(print());
+    }
+
+
+
+
+}
